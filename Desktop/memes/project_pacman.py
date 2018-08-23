0ab7f2a323c11405d5d7f0bc5f04f9eef6efb1e4
Kristoffer van der Weele Gjøen <pigemajig@gmail.com>
2018-08-23 11:07:53 +0200

    trial commit

---
+++ Desktop/memes/project_pacman.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.linspace(0,10,11)
+y=np.linspace(0,10,11)
+xy=np.zeros(11)
+for i in range(0,len(xy)):
+    xy[i]=np.linspace(0,10,11)
+
+
+#for i in x:
+#    for j in y:
+#        xy[i,j]=i,j
+print(xy)
